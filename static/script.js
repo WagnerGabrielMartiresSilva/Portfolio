24c8b925af31cf8fb63002f85459feeed0812ce6
wagne <wagner@gmail.com>
2022-06-05 23:37:05 -0300

    menu habilidades

--- static/script.js
+++ static/script.js
@@ -7,5 +7,18 @@ menuMobile.addEventListener("click", () =>{
     menuMobile.classList.contains("bi-list")
     ?menuMobile.classList.replace("bi-list", "bi-x") /* --- ? significa if----*/
     :menuMobile.classList.replace("bi-x","bi-list"); /*---- : significa else---*/
-    body.classList.toggle("menu-nav-activate")
+    body.classList.toggle("menu-nav-activate")       /*---- ativar a barra lateral ou seja o menu-nav */
 });
+
+/* Fecha o menu quando clicar em algum item e muda o icone para list */
+
+const navItem= document.querySelectorAll(".nav-item")
+
+navItem.forEach(item => {
+    item.addEventListener("click",()=> {
+        if (body.classList.contains("menu-nav-activate")) {
+            body.classList.remove("menu-nav-activate")
+            menuMobile.classList.replace("bi-x","bi-list");
+        }
+    })
+})
